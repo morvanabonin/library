8e493a75ef5da4aa288d852122e50d8ab067e1fb
Morvana <morvanabonin@gmail.com>
2015-09-14 23:16:32 -0300

    Arquivos do Controller e lógica de rotas.

---
+++ app/controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* Class of system user
+*/
+class User
+{
+    public function indexAction()
+    {
+        # code...
+    }
+
+    public function addAction() {
+        echo "Work, new User";
+    }
+
+    public function editAction() {
+
+    }
+
+    public function deleteAction() {
+
+    }
+}
